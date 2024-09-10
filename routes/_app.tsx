3c1f18116fd25b1e1f13e9551f403d281c47b7f2
p3bozuric <166792070+p3bozuric@users.noreply.github.com>
2024-09-10 13:56:52 +0200

    Metadata
    Editing of metadata

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,49 +9,46 @@ function MetaTags(
     <>
       {/* Primary Meta Tags */}
       <title>
-        ${credentials.name} - Experienced Web Developer | Full Portfolio
+        {credentials.name} - AI Developer | Machine Learning, Computer Vision, NLP
       </title>
       <meta
         name="title"
-        content={`${credentials.name} - Experienced Web Developer | Full Portfolio`}
+        content={`${credentials.name} - AI Developer | Machine Learning, Computer Vision, NLP`}
       />
       <meta
         name="description"
-        content={`Explore ${credentials.name}'s portfolio showcasing a diverse range of web development projects. Specializing in full-stack development with expertise in JavaScript, React, Node.js, and more. Discover how I can bring your web vision to life.`}
+        content={`Explore ${credentials.name}'s portfolio showcasing innovative AI projects. Specializing in machine learning, computer vision, and NLP using Python. Discover how I can bring your AI vision to life.`}
       />
-
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={`${url}`} />
       <meta
         property="og:title"
-        content={`${credentials.name} - Experienced Web Developer | Full Portfolio`}
+        content={`${credentials.name} - AI Developer | Machine Learning, Computer Vision, NLP`}
       />
       <meta
         property="og:description"
-        content={`Explore ${credentials.name}'s portfolio showcasing a diverse range of web development projects. Specializing in full-stack development with expertise in JavaScript, React, Node.js, and more. Discover how I can bring your web vision to life.`}
+        content={`Explore ${credentials.name}'s portfolio showcasing innovative AI projects. Specializing in machine learning, computer vision, and NLP using Python. Discover how I can bring your AI vision to life.`}
       />
       <meta
         property="og:image"
         content={`${url}meta-image.webp`}
       />
-
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={`${url}`} />
       <meta
         property="twitter:title"
-        content={`${credentials.name} - Experienced Web Developer | Full Portfolio`}
+        content={`${credentials.name} - AI Developer | Machine Learning, Computer Vision, NLP`}
       />
       <meta
         property="twitter:description"
-        content={`Explore ${credentials.name}'s portfolio showcasing a diverse range of web development projects. Specializing in full-stack development with expertise in JavaScript, React, Node.js, and more. Discover how I can bring your web vision to life.`}
+        content={`Explore ${credentials.name}'s portfolio showcasing innovative AI projects. Specializing in machine learning, computer vision, and NLP using Python. Discover how I can bring your AI vision to life.`}
       />
       <meta
         property="twitter:image"
         content={`${url}meta-image.webp`}
       />
-
       {/* Meta Tags Generated with https://metatags.io */}
     </>
   );
@@ -61,23 +58,19 @@ export default function App({ Component }: PageProps) {
   const socials = [
     {
       name: "GitHub",
-      url: "https://github.com/michael-pfister/deno-portfolio",
+      url: "https://github.com/p3bozuric",
       icon: "fa-brands:github",
     },
-    {
-      name: "Twitter",
-      url: "https://twitter.com/ScaredToCompile",
-      icon: "fa-brands:twitter",
-    },
+
     {
       name: "LinkedIn",
-      url: "https://www.linkedin.com/in/michael-pascal-pfister",
+      url: "https://www.linkedin.com/in/pbozuric/",
       icon: "fa-brands:linkedin",
     },
   ];
 
   const credentials = {
-    name: "John Doe",
+    name: "Patrik Bozuric",
   };
 
   const url = "https://opensource-portfolio.deno.dev/";
